4478577094bf5c96f350f068cc8f7bd9ba514722
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2016-05-17 03:45:48 +0300

    First shot

---
+++ S650_ROW-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lenovo/S650_ROW/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lenovo/S650_ROW/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lenovo/S650_ROW/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lenovo/S650_ROW/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lenovo/S650_ROW/xbin,system/xbin)
+
